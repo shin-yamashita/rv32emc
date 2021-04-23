c5b1bbeb51536c54b458b7c5b6916afd616a62fb
S.Yamashita <s.yamashita@jcom.zaq.ne.jp>
2021-04-23 15:20:43 +0900

    fix correct synth source

--- rv_core/syn/read_hdl.tcl
+++ rv_core/syn/read_hdl.tcl
@@ -7,10 +7,11 @@ read_verilog -library xil_defaultlib -sv {
   ../hdl/rv_muldiv.sv
   ../hdl/rv_regf.sv
   ../hdl/rv_core.sv
+  ../hdl/dpram.sv
   ../sim/rvc.sv
 }
 read_verilog -library xil_defaultlib {
- ../hdl/dpram.v
+  ../hdl/dpram_h.v
 }
 
 set_property file_type "Verilog Header" [get_files ../hdl/rv_types.svh]
