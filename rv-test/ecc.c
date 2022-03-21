7535046d5cdc1a67270c5d20cfb334d1e9282335
Shinichi Yamashita <s.yamashita@jcom.zaq.ne.jp>
2022-03-21 19:14:36 +0900

    fix bug mod255(), p_mod()

--- rv-test/ecc.c
+++ rv-test/ecc.c
@@ -1,6 +1,6 @@
 //
 // ecc.c
-// Reed Solomon Coding 
+// Reed Solomon Coding
 // primitive polynomial : X^8 + X^4 + X^3 + X^2 + 1
 //
 
@@ -8,32 +8,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "ulib.h"
-
-//------------------------------------------------------
-extern char _end[];                /* _end is set in the linker command file */
-char *heap_ptr;
-/*
- * sbrk -- changes heap size size. Get nbytes more
- *         RAM. We just increment a pointer in what's
- *         left of memory on the board.
- */
-#ifdef __cplusplus
-extern "C" char *_sbrk (int nbytes);
-#endif
-
-char *_sbrk (int nbytes)
-{
-  char        *base;
-
-  if (!heap_ptr)
-    heap_ptr = (char *)&_end;
-  base = heap_ptr;
-  heap_ptr += nbytes;
-
-  return base;
-}
-//------------------------------------------------------
 
 #define MAXN	256
 #define MAXV	255
@@ -44,7 +18,7 @@ typedef uint8_t GFN;	// GF(2^8) Galois extension
 
 typedef struct {	// Galois extension Polynomial
   int n;
-  GFN *x;
+  GFN x[MAXN];
 } Poly_t;
 
 typedef struct {	// error location / pattern data
@@ -100,7 +74,8 @@ GFN mul(GFN a, GFN b)	// a * b
 
 int mod255(int x)
 {
-  return x >= 255 ? x - 255 : x;
+  while(x >= 255) x -= 255;
+  return x;
 }
 
 GFN vmul(GFN a, GFN b)	// a * b
@@ -155,41 +130,17 @@ void p_print(const char *msg, Poly_t *p, int mode)
   printf("\n");
 }
 
-Poly_t *p_new(int n)	// n deg polynom
+Poly_t *p_clear(Poly_t *p, int n)	// n deg polynom
 {
-  Poly_t *a;
-  a = (Poly_t *)malloc(sizeof(Poly_t));
-  a->n = n < 0 ? 0 : n;
-  a->x = (GFN *)calloc(a->n+1, sizeof(GFN));
-  return a;
-}
-
-void p_free(Poly_t *a)
-{
-  if(a && a->x){
-    free(a->x);
-    free(a);
-  }
-}
-
-Poly_t *p_resize(Poly_t *y, int n)	// resize
-{
-  int i;
-  if(!y){
-    y = p_new(n);
-  }else{
-    n = n < 0 ? 0 : n;
-    if(n+1 > y->n)
-      y->x = (GFN*)realloc(y->x, n+1);
-    for(i = 0; i <= n; i++) y->x[i] = 0;
-    y->n = n;
-  }
-  return y;
+    int i;
+    p->n = n;
+    for(i = 0; i <= n; i++) p->x[i] = 0;
+    return p;
 }
 
 Poly_t *p_cpy(Poly_t *a, Poly_t *y)
 {
-  y = p_resize(y, a->n);
+  y = p_clear(y, a->n);
   memcpy(y->x, a->x, a->n+1);
   return y;
 }
@@ -203,9 +154,8 @@ Poly_t *p_add(Poly_t *a, Poly_t *b, Poly_t *y)	// a + b => y
 {
   int i;
   int n = a->n > b->n ? a->n : b->n;
-  static Poly_t *tmp = NULL;
-
-  tmp = p_resize(tmp, n);
+  Poly_t _tmp;
+  Poly_t *tmp = p_clear(&_tmp, n);
   for(i = 0; i <= n; i++){
     tmp->x[i] = elem(a, i) ^ elem(b, i);
   }
@@ -216,9 +166,8 @@ Poly_t *p_mul(Poly_t *a, Poly_t *b, Poly_t *y)  // a * b => y
 {
   int i, j;
   int n = a->n + b->n;
-  static Poly_t *tmp = NULL;
-
-  tmp = p_resize(tmp, n); 
+  static Poly_t _tmp;
+  Poly_t *tmp = p_clear(&_tmp, n); 
   for(i = 0; i <= a->n; i++){
     for(j = 0; j <= b->n; j++){
       tmp->x[i+j] ^= vmul(elem(a,i), elem(b,j));
@@ -227,13 +176,13 @@ Poly_t *p_mul(Poly_t *a, Poly_t *b, Poly_t *y)  // a * b => y
   return p_cpy(tmp, y);
 }
 
-Poly_t *p_mul_xn(Poly_t *a, GFN x, int n, Poly_t *y)  // a * s x^n => y
+Poly_t *p_mul_xn(Poly_t *a, GFN x, int n, Poly_t *y)  // a * x^n => y
 {
   int i;
   int m = a->n + n;
-  static Poly_t *tmp = NULL;
+  Poly_t _tmp;
 
-  tmp = p_resize(tmp, m);
+  Poly_t *tmp = p_clear(&_tmp, m);
   for(i = 0; i <= a->n; i++)
     tmp->x[i+n] = vmul(a->x[i], x);
 
@@ -256,8 +205,9 @@ Poly_t *p_mod(Poly_t *a, Poly_t *b, Poly_t *q, Poly_t *r)	// a / b => q ... r
   int m = b->n;
   GFN qe;
   r = p_cpy(a, r);
-
-  Poly_t *xr = p_new(r->n);
+  Poly_t _xr;
+  Poly_t *xr = p_clear(&_xr, r->n);
+  if(q) q->n = n-m;
   while(n >= m){
     qe = vdiv(r->x[n], b->x[m]);
     xr = p_mul_xn(b, qe, n-m, xr);
@@ -268,12 +218,13 @@ Poly_t *p_mod(Poly_t *a, Poly_t *b, Poly_t *q, Poly_t *r)	// a / b => q ... r
   return r;
 }
 
-Poly_t *encode(Poly_t *info, int npar)
+Poly_t *encode(Poly_t *info, Poly_t *code, int npar)
 {
-  Poly_t *code = p_new(info->n + npar);
-  Poly_t *G = p_new(0);
-  Poly_t *r = p_new(0);
-  Poly_t *g = p_new(1);
+  p_clear(code, info->n + npar);
+  Poly_t _G, _r, _g;
+  Poly_t *G = p_clear(&_G, 0);
+  Poly_t *r = p_clear(&_r, 0);
+  Poly_t *g = p_clear(&_g, 1);
   int i;
   G->x[0] = 1;	// G(x) Generator polynomial (x-a^0)(x-a^1)(x-a^3)...(x-a^(npar-1))
   g->x[1] = 1;	// x-a^0
@@ -291,10 +242,10 @@ Poly_t *encode(Poly_t *info, int npar)
   return code;
 }
 
-Poly_t *syndrome(Poly_t *code, int npar)
+Poly_t *syndrome(Poly_t *code, Poly_t *sy, int npar)
 {
   int i, j;
-  Poly_t *sy = p_new(npar-1);
+  p_clear(sy, npar-1);
 
   for(i = 0; i <= code->n; i++){
     for(j = 0; j <= sy->n; j++){
@@ -316,10 +267,10 @@ GFN p_eval(Poly_t *a, GFN x)
   return ev;
 }
 
-Poly_t *findroot(Poly_t *a)
+Poly_t *findroot(Poly_t *a, Poly_t *ea)
 {
   int i, n = 0;
-  Poly_t *ea = p_new(a->n);
+  p_clear(ea, a->n);
   GFN ev;
   for(i = 0; i <= 255; i++){
     ev = p_eval(a, gexp[i]);
@@ -334,32 +285,39 @@ Poly_t *findroot(Poly_t *a)
 int decode(Poly_t *s, errdata_t *edat)
 {
   int i;
-  Poly_t *A   = p_new(s->n+1);
-  Poly_t *B   = p_cpy(s, NULL);
-  Poly_t *r   = NULL;
-  Poly_t *ea  = NULL;
-  Poly_t *q   = p_new(s->n+1);
-  Poly_t *b   = p_new(0);
-  Poly_t *sgm = p_new(0);
-  Poly_t *sgm0 = p_new(0);
-  Poly_t *tmp = p_new(0);
+  Poly_t _A, _B, _r, _ea, _q, _b, _sgm, _sgm0, _tmp;
+  Poly_t *A   = p_clear(&_A, s->n+1);
+  Poly_t *B   = p_cpy(s, &_B);
+  Poly_t *r   = &_r;
+  Poly_t *ea  = &_ea;
+  Poly_t *q   = p_clear(&_q, s->n+1);
+  Poly_t *b   = p_clear(&_b, 0);
+  Poly_t *sgm = p_clear(&_sgm, 0);
+  Poly_t *sgm0 = p_clear(&_sgm0, 0);
+  Poly_t *tmp = p_clear(&_tmp, 0);
 
   A->x[A->n] = 1;	// x^(n+1)
   sgm->x[0] = 1;
 
-  while(B->n >= 4){	// Euclid's Algorithm
-    r = p_mod(A, B, q, NULL);
-
+  do{   // Euclid's Algorithm
+    r = p_mod(A, B, q, r);
+    p_reduc(r);
+//p_print("  A: ", A, 1);
+//p_print("  B: ", B, 1);
+//p_print("  q: ", q, 1);
+//p_print("  r: ", r, 1);
     tmp = sgm;
     b = p_mul(sgm, q, b);
     sgm = p_add(sgm0, b, sgm0);
     sgm = p_reduc(sgm);
     sgm0 = tmp;
+//p_print("sgm: ", sgm, 1);
+    A = p_cpy(B, A);
+    B = p_cpy(r, B); // omega
 
-    ea = findroot(sgm);
-    A = B;
-    B = p_reduc(r);
-  }
+  }while(sgm->n < B->n);
+
+  ea = findroot(sgm, &_ea);
 //  p_print(" omega: ", B, 1);
 //  p_print(" sigma: ", sgm, 1);
   // ei = w(a^-ji)/dsgm(a^-ji)
@@ -372,7 +330,7 @@ int decode(Poly_t *s, errdata_t *edat)
   for(i = 0; i < ea->n; i++){
     aj = ea->x[i];
     ep = vdiv(p_eval(B, aj), p_eval(sgm, aj));
-    edat->ea[i] = 255 - glog[aj];
+    edat->ea[i] = mod255(255 - glog[aj]);
     edat->ep[i] = ep;
   }
 
@@ -396,11 +354,12 @@ GFN prsg()
 }
 
 #define Ni	32
-#define Np	8
+#define Np	12
 
 int main()
 {
   int i, j;
+  Poly_t _info, _code, _Synd;
   Poly_t *info, *code, *Synd;
 
 //  init_timer(100000);   // 10 us
@@ -409,31 +368,31 @@ int main()
 
   root_table();
 
-  info = p_new(Ni-1);
+  info = p_clear(&_info, Ni-1);
 
   for(i = 0; i < Ni; i++){
     info->x[info->n-i] = i;
   }
   p_print("info :", info, 1);
 
-  code = encode(info, Np);
+  code = encode(info, &_code, Np);
   p_print("code :", code, 1);
 
-  Synd = syndrome(code, Np);
+  Synd = syndrome(code, &_Synd, Np);
   p_print("Synd :", Synd, 1);
 
   int ea;
   GFN ep;
   errdata_t edat;
 
-  for(i = 0; i < 4; i++){
+  for(i = 0; i < Np/2; i++){
     ea = prsg() % (Ni+Np);
     ep = prsg();
 
     code->x[ea] ^= ep;
     printf("+++++ %d err  %2x @ %d\n", i+1, ep, ea);
     p_print("code :", code, 1);
-    Synd = syndrome(code, Np);
+    Synd = syndrome(code, &_Synd, Np);
     p_print("Synd :", Synd, 1);
 
     decode(Synd, &edat);
@@ -449,12 +408,11 @@ int main()
   for(i = 0; i < edat.n; i++){	// correct
     code->x[edat.ea[i]] ^= edat.ep[i];
   }
-  Synd = syndrome(code, Np);
+  Synd = syndrome(code, &_Synd, Np);
   p_print("code :", code, 1);
   p_print("Synd :", Synd, 1);
 
   printf("**** %s\n", is_zero(Synd) ? "No error." : "Error");
-  printf("heap used : %d B\n", heap_ptr - _end);
 
 }
 
